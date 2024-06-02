7b474589071699a92b52f73c0c74c8d54802a03f
Kang-heeju <hjkang0107@gmail.com>
2024-06-02 15:21:35 +0900

    add user module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/:id")
+  getHello(@Param("id") id: string) {
+    console.log(id);
+    // return this.appService.getHello();
+  }
+
+  @Post()
+  getPost(@Body() body: any) {
+    console.log(body);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { typeORMConfig } from './config/typeorm.config';
 import { CommentsModule } from './comments/comments.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [BoardsModule, CommentsModule],
+  imports: [BoardsModule, CommentsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
